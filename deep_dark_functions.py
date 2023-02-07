87fefb7a6c95fce91bc5e4c3035febba64b5a877
Vlad <homyak72088@gmail.com>
2023-02-07 17:16:06 +0200

    refactored

--- deep_dark_functions.py
+++ deep_dark_functions.py
@@ -10,20 +10,20 @@ def millimeters_to_inches(x: Union[float, int]) -> float:
 
 
 def area_of_the_paper(x: Union[list, tuple]) -> str:
-    z = millimeters_to_inches(x[1]) * millimeters_to_inches(x[2])
-    c = f'Paper {x[0]} has area {z} square inches'
-    return c
+    value = millimeters_to_inches(x[1]) * millimeters_to_inches(x[2])
+    area = f'Paper {x[0]} has area {value} square inches'
+    return area
 
 
-temp_lambda = list(map(area_of_the_paper, Lists.paper_list))
+temp_lambda = list(map(area_of_the_paper, lists.paper_list))
 
 temp_lambda = list(
     map(lambda x: f'Paper {x[0]} has area {millimeters_to_inches(x[1]) * millimeters_to_inches(x[2])} square inches',
-        Lists.paper_list))
+        lists.paper_list))
 
 
 def filter_temp(x: str) -> bool:
     return 'A' in x
 
 
-aaaa = filter(filter_temp, temp_lambda)
+list_sorted = filter(filter_temp, temp_lambda)
